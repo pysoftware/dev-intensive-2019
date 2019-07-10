b9a2b92d12294201c9a8494854031f7a5bb90c33
carebearlover1 <sqzonow@mail.ru>
2019-07-10 08:42:37 -0700

    а

--- app/src/main/java/com/magere/dev_intensive_2019/models/UserView.kt
+++ app/src/main/java/com/magere/dev_intensive_2019/models/UserView.kt
@@ -3,9 +3,10 @@ package com.magere.dev_intensive_2019.models
 import android.provider.ContactsContract
 
 class UserView(
+
     val id:String,
-    val fullName:String,
-    val nickName:String,
+    val fullName:String?,
+    val nickName:String?,
     val avatar:String? = null,
     val status:String? = "offline",
     val initials:String?
